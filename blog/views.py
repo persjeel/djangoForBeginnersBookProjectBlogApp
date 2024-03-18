7af8c6f2e810a3271a9ca094f0c617bfdd4f8753
Annie <persjeel@gmail.com>
2024-03-18 16:17:46 +0200

    added blog deletion

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -29,3 +30,8 @@ class BlogUpdateView(UpdateView):
     # specified database fields to expose in edit form
     fields = ['title', 'body']
 
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
